486893e28d1df76ccbcb9b5b262068eca722564b
kejsiblushi <kejsi.blushi5@gmail.com>
2023-12-06 13:14:08 +0100

    feat: alternative js functions

--- card.js
+++ card.js
@@ -1,7 +1,26 @@
 function openPopup() {
-  document.getElementById("popup").style.display = "flex";
+  var popup = document.querySelector(".popup");
+  popup.style.display = "flex";
 }
 
 function closePopup() {
   document.getElementById("popup").style.display = "none";
 }
+
+//Another way (remove onclick attribute in button element)
+// var openButton = document.getElementById("open-button");
+// openButton.addEventListener("click", function(){
+//   document.getElementById("popup").style.display = "flex";
+// });
+
+// var closeButton = document.getElementById("close-button");
+// closeButton.addEventListener("click", function(){
+//   document.getElementById("popup").style.display = "none";
+// });
+
+// Another way (using predifined functions and remove onclick attribute in button element)
+// var openButton = document.getElementById("open-button");
+// openButton.addEventListener("click", openPopup);
+
+// var closeButton = document.getElementById("close-button");
+// closeButton.addEventListener("click", closePopup);
